ba4b9a653306d67ae4ba028b40419af6bcf8b2d5
sldless <67599596+sldless@users.noreply.github.com>
2021-11-11 20:21:33 -0500

    Update ready.ts

--- src/handlers/events/ready.ts
+++ src/handlers/events/ready.ts
@@ -3,6 +3,6 @@ import { Event } from '../../interfaces';
 export const event: Event = {
     name: 'ready',
     run: client => {
-        console.log(client.user.tag, + ' is now ready!');
+        console.log(`${client.user.tag} is online with the server count of ${client.guilds.size}`);
     }
 }
